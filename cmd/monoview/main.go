465c0f574fc450d25d723dadb44b7a41a2dfb272
MrZloHex <zlo.alex.it@gmail.com>
2026-01-23 23:39:14 +0300

    qwe

---
+++ cmd/monoview/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "time"
+
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/bubbles/textinput"
+    "github.com/charmbracelet/lipgloss"
+    "github.com/gorilla/websocket"
+)
+
+
+func main() {
+}
+
